d3a8963f9c568079bbfe36c3cb9db7d17a1c5fbc
Avens_Tach <artur.tymchuk@gmail.com>
2024-01-23 23:41:11 +0200

    Initial commit

---
+++ bullet.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Bullet(pygame.sprite.Sprite):
+
+
+    def __init__(self, screen, gun):
+        """create bullet"""
+        super(Bullet, self).__init__()
+        self.screen = screen
+        self.rect = pygame.Rect(0, 0, 300, 12)
+        self.color = 76, 175, 79
+        self.speed = 2
+        self.rect.centerx = gun.rect.centerx
+        self.rect.top = gun.rect.top
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """bullet move up"""
+        self.y -= self.speed
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """bullet draw in screen"""
+        pygame.draw.rect(self.screen, self.color, self.rect)
---
+++ ino.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Ino(pygame.sprite.Sprite):
+    """1 alien"""
+
+    def __init__(self, screen):
+        """initialization"""
+        super(Ino, self).__init__()
+        self.screen = screen
+        self.image = pygame.image.load('image/pixil-frame-1.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+
+    def draw(self):
+        """draw ino"""
+        self.screen.blit(self.image, self.rect)
+
+
+
+    def update(self):
+        """move inos"""
+        self.y += 0.05
+        self.rect.y = self.y
+
+
